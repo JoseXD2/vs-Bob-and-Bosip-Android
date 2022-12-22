a1c505aba7ecf0576b00a8c0b0c2e9b03e1302d0
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2022-12-22 09:46:28 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -143,7 +143,25 @@ class DownscrollOption extends Option
 		return FlxG.save.data.downscroll ? "Downscroll" : "Upscroll";
 	}
 }
+class AndroidControls extends Option
+{
+	public function new()
+	{
+		super();
+	}
 
+	public override function press():Bool
+	{
+		trace("switch");
+		FlxG.switchState(new android.AndroidControlsMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Android Controls";
+	}
+}
 class EEE extends Option
 {
 	public function new(desc:String)
